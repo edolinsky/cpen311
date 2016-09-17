9940d2b7717db4e805f72ceffb8cdd6b6db11afc
reoliveira <oliveira.reid@gmail.com>
2016-09-16 20:37:57 -0700

    completed datapath and added a register component

--- lab1/datapath.v
+++ lab1/datapath.v
@@ -11,15 +11,29 @@ input load_dcard1, load_dcard2, load_dcard3;
 output [3:0] pcard3_out;
 output [3:0] pscore_out, dscore_out;
 output [6:0] HEX5, HEX4, HEX3, HEX2, HEX1, HEX0;
-						
 
-// The code describing your datapath will go here.  Your datapath 
-// will hierarchically instantiate six card7seg blocks, two scorehand
-// blocks, and a dealcard block.  The registers may either be instatiated
-// or included as sequential always blocks directly in this file.
-//
-// Follow the block diagram in the Lab 1 handout closely as you write this code
-	
-	
+wire [3:0] new_card;
+wire [3:0] pcard1, pcard2, pcard3, dcard1, dcard2, dcard3;
+						
+dealcard dealcard (.clock(slow_clock), .resetb(resetb), .new_card(new_card));
+
+reg4 preg1 (.clk(slow_clock), .reset(resetb), .load(load_pcard1), .in(new_card), .out(pcard1));
+reg4 preg2 (.clk(slow_clock), .reset(resetb), .load(load_pcard2), .in(new_card), .out(pcard2));
+reg4 preg3 (.clk(slow_clock), .reset(resetb), .load(load_pcard3), .in(new_card), .out(pcard3));
+reg4 dreg1 (.clk(slow_clock), .reset(resetb), .load(load_dcard1), .in(new_card), .out(dcard1));
+reg4 dreg2 (.clk(slow_clock), .reset(resetb), .load(load_dcard2), .in(new_card), .out(dcard2));
+reg4 dreg3 (.clk(slow_clock), .reset(resetb), .load(load_dcard3), .in(new_card), .out(dcard3));
+
+card7seg pcard1_display (.card(pcard1), .seg7(HEX0));
+card7seg pcard2_display (.card(pcard2), .seg7(HEX1));
+card7seg pcard3_display (.card(pcard3), .seg7(HEX2));
+card7seg dcard1_display (.card(pcard1), .seg7(HEX3));
+card7seg dcard2_display (.card(dcard2), .seg7(HEX4));
+card7seg dcard3_display (.card(dcard3), .seg7(HEX5));
+
+scorehand playerscore (.card1(pcard1), .card2(pcard2), .card3(pcard3), .total(pscore_out));
+scorehand dealerscore (.card1(pcard1), .card2(pcard2), .card3(pcard3), .total(dscore_out));
+
+assign pcard3_out = pcard3;
 
 endmodule
---
+++ lab1/reg4.v
@@ -0,0 +1,13 @@
+module reg4 (clk, reset, load, in, out);
+
+	input clk, reset, load;
+	input [3:0] in;
+	output reg [3:0] out;
+	
+	always_ff @(posedge clk or posedge reset)
+		if (reset == 1)
+			out <= 0;
+		else 
+			if (load == 1)
+				out <= in;
+endmodule
