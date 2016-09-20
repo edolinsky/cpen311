0c03d38a64f5a00d87ff69df07bedf922426a48e
reoliveira <oliveira.reid@gmail.com>
2016-09-20 12:05:46 -0700

    datapath with betting

--- lab1/reg4.v
+++ lab1/reg4.v
@@ -1,11 +1,11 @@
-module reg4 (clk, reset, load, in, out);
+module reg4 (clk, reset, reset_cards, load, in, out);
 
-	input clk, reset, load;
+	input clk, reset, reset_cards, load;
 	input [3:0] in;
 	output reg [3:0] out;
 	
-	always_ff @(posedge clk or negedge reset)
-		if (reset == 0)
+	always_ff @(posedge clk or negedge reset or posedge reset_cards)
+		if (reset == 0 | reset_cards == 1)
 			out <= 0;
 		else 
 			if (load == 1)
