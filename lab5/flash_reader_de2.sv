51d5a81ff2f1302f937c1195de4829938f513196
reoliveira <oliveira.reid@gmail.com>
2016-11-15 15:15:11 -0800

    task 4 attempt

--- lab5/flash_reader_de2.sv
+++ lab5/flash_reader_de2.sv
@@ -1,13 +1,18 @@
-module flash_reader_de2( CLOCK_50, KEY,
+module flash_reader_de2( CLOCK_50, resetb,
     FL_ADDR,
     FL_CE_N,
     FL_DQ,
     FL_OE_N,
     FL_RST_N,
-    FL_WE_N);
+    FL_WE_N,
+	 data,
+	 valid,
+	 next,
+	 done
+	 );
   
 input CLOCK_50;
-input [3:0] KEY;
+input resetb;
 
 // Flash memory I/O
 // low enable for all signals
@@ -18,13 +23,12 @@ output FL_OE_N;			// Flash output enable
 output FL_RST_N;			// Flash hardware reset
 output FL_WE_N;			// Flash write enable
 
-// State machine states
-enum {FL_INIT, FL_READ, FL_WAIT, FL_LOAD, FL_LOOP_CONTROL, DONE} state;
+output [15:0] data;
+output valid, done;
+input next;
 
-// clock & reset
-wire clk, resetb;
-assign clk = CLOCK_50;
-assign resetb = KEY[3];
+// State machine states
+enum {FL_INIT, FL_READ, FL_WAIT, FL_LOAD, FL_LOOP_CONTROL, WAIT, DONE} state;
 
 // Do not need to reset, nor write to flash
 assign FL_WE_N = 1'b1;	// high write enable for flash read
@@ -35,16 +39,9 @@ parameter WAIT_CYCLES = 3'd6; // minimum 110ns read time, so wait 6 cycles in be
 reg [2:0] wait_reg;	// register for number of cycles in 
 
 // RAM & intermediary registers
-reg s_wren;
-reg [7:0] s_address;
-reg [15:0] s_data, s_q, f_data;
+reg [7:0] f_address;
+reg [15:0] f_data;
 reg addr_offset;
-
-s_memory s0(.address(s_address),
-				.clock(CLOCK_50),
-				.data(s_data),
-				.wren(s_wren),
-				.q(s_q));
 
 always_ff @(posedge CLOCK_50, negedge resetb) begin
 
@@ -58,9 +55,13 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 		 * This state initializes registers needed for loading data from flash
 		 */
 		FL_INIT: begin
-			s_address <= 8'd0;	// initialize address to 0
+			f_address <= 8'd0;	// initialize address to 0
 			addr_offset <= 1'b0;	// initialize flash offset address to 0
 			wait_reg <= 3'd0;		// initialize wait counter to 0
+			
+			data <= 16'd0; // init data to 0
+			valid <= 1'b0; // init valid flag to 0
+			done <= 1'b0;
 			
 			state <= FL_READ;
 		end // case FL_INIT
@@ -73,7 +74,7 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 			FL_CE_N <= 1'b0;
 			
 			// can only read 8 bits at a time, so read each byte separately
-			FL_ADDR <= (2'd2 * s_address) + addr_offset;
+			FL_ADDR <= (2'd2 * f_address) + addr_offset;
 			
 			state <= FL_WAIT;
 		end // case FL_READ
@@ -98,7 +99,7 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 		
 		/*	FL_LOAD:
 		 *	This state loads the requested byte from flash into an internal register
-		 * and if the full sample (2 bytes) has been read, loads the sample into memory
+		 * and if the full sample (2 bytes) has been read, places the value on the data bus
 		 */
 		FL_LOAD: begin
 			
@@ -113,12 +114,12 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 				
 				state <= FL_READ;
 				
-			// if offset is 1, full sample has been read; load into memory
+			// if offset is 1, full sample has been read;
 			end else begin
 				addr_offset <= 1'b0;
 				
-				s_wren <= 1'b1;
-				s_data <= f_data;
+				valid <= 1'b1;
+				data <= f_data;
 				
 				state <= FL_LOOP_CONTROL;
 			end // if
@@ -130,21 +131,30 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 		 */
 		FL_LOOP_CONTROL: begin
 			
-			if (s_address < 8'd255) begin
-				s_address <= s_address + 1'b1;
-				state <= FL_READ;
+			if (f_address < 8'd255) begin
+				f_address <= f_address + 1'b1;
+				state <= WAIT;
 			end else begin
 				state <= DONE;
 			end // if
 		
 		end // case FL_LOOP_CONTROL
 		
+		/* WAIT:
+		 * This states waits for input to instruct the module to load the next value
+		 */
+		WAIT: begin
+			if (next == 1'b1) begin
+				valid <= 0; // the data is no longer the next valid value
+				state <= FL_READ; // read next value
+			end
+		end // case WAIT
+		
 		DONE: begin
 			state <= DONE;
+			done <= 1'b1;
 		end // case DONE
 		
-	
-	
 	endcase
 	
 	
---
+++ lab5/lab5.v
@@ -0,0 +1,125 @@
+module lab5(CLOCK_50, CLOCK_27, KEY, FL_ADDR, FL_CE_N, FL_DQ, FL_OE_N, FL_RST_N, FL_WE_N);
+
+input CLOCK_50;
+input CLOCK_27;
+input [3:0] KEY;
+
+output [21:0] FL_ADDR;	// Flash address bus
+output FL_CE_N;			// Flash Chip enable
+inout[7:0] FL_DQ;			// Flash Data input/output
+output FL_OE_N;			// Flash output enable
+output FL_RST_N;			// Flash hardware reset
+output FL_WE_N;			// Flash write enable
+
+// reset
+assign resetb = KEY[3];
+
+// State machine states
+enum {WAIT_READ_READY, WAIT_WRITE_READY, SEND_SAMPLE, WAIT_ACCEPT, DONE} state;
+
+
+// signals that are used to communicate with the audio core
+
+reg read_ready, write_ready, write_s;
+reg [15:0] writedata_left, writedata_right;
+reg [15:0] readdata_left, readdata_right;	
+wire reset, read_s;
+
+// instantiate the parts of the audio core. 
+
+clock_generator my_clock_gen (CLOCK_27, reset, AUD_XCK);
+audio_and_video_config cfg (CLOCK_50, reset, I2C_SDAT, I2C_SCLK);
+audio_codec codec (CLOCK_50,reset,read_s,write_s,writedata_left, writedata_right,AUD_ADCDAT,AUD_BCLK,AUD_ADCLRCK,AUD_DACLRCK,read_ready, write_ready,readdata_left, readdata_right,AUD_DACDAT);
+
+// flash reader signals
+wire [15:0] f_data, sample;
+wire valid, next, done;
+
+flash_reader_de2 flash_reader(
+	.CLOCK_50(CLOCK_50),
+	.resetb(resetb),
+	.FL_ADDR(FL_ADDR),
+	.FL_CE_N(FL_CE_N),
+	.FL_DQ(FL_DQ),
+	.FL_OE_N(FL_OE_N),
+	.FL_RST_N(FL_RST_N),
+	.FL_WE_N(FL_WE_N),
+	.data(f_data),
+	.valid(valid),
+	.next(next),
+	.done(done));
+
+always_ff @(posedge CLOCK_50, negedge resetb) begin
+
+	if(resetb == 0) begin
+		next <= 1'b0;
+		state <= WAIT_READ_READY;
+	end else begin
+	
+	case(state)
+	
+		/* WAIT_READ_READY:
+		 * waits for a value to be ready from flash memory
+		 */
+		WAIT_READ_READY: begin
+			write_s <= 1'b0;
+			
+			if (done) begin
+				state <= DONE;
+			end else if (valid == 1) begin
+				sample <= f_data; // record the data provided
+				next <= 1'b1; // start reading the next sample
+				state <= WAIT_WRITE_READY;
+			end else begin
+				state <= WAIT_READ_READY;
+			end
+			
+		end
+		
+		/* WAIT_WRITE_READY:
+		 * waits for the audio core to be ready for a sample
+		 * and the flash memory to have started its next read.
+		 */
+		WAIT_WRITE_READY: begin
+			if (write_ready == 1'b1 && valid == 1'b0) begin
+				next <= 1'b0;
+				state <= SEND_SAMPLE;
+			end else begin
+				state <= WAIT_WRITE_READY;
+			end
+		end
+		
+		/* SEND_SAMPLE:
+		 * sends a sample to the audio core
+		 */
+		SEND_SAMPLE: begin
+			writedata_right <= sample;
+			writedata_left <= sample;
+			write_s <= 1'b1;  // indicate we are writing a value
+			state <= WAIT_ACCEPT;
+		end
+		
+		/* WAIT_ACCEPT:
+		 * waits for the sample to be accepted by the audio core
+		 */
+		WAIT_ACCEPT: begin
+			if (write_ready == 1'b0) begin
+				state <= WAIT_READ_READY;
+			end
+		end
+		
+		DONE: begin
+			state <= DONE;
+		end
+		
+		default: begin
+			state <= WAIT_READ_READY;
+		end
+		
+	endcase
+	
+	end
+	
+end
+
+endmodule
