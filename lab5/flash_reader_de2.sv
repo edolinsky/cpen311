b615090b3c2d8966ed113e7e391edea793bce6f7
reoliveira <oliveira.reid@gmail.com>
2016-11-17 01:25:10 -0800

    working impl

--- lab5/flash_reader_de2.sv
+++ lab5/flash_reader_de2.sv
@@ -1,13 +1,10 @@
-module flash_reader_de2( CLOCK_50, KEY,
-    FL_ADDR,
-    FL_CE_N,
-    FL_DQ,
-    FL_OE_N,
-    FL_RST_N,
-    FL_WE_N);
-  
-input CLOCK_50;
+module flash_reader_de2( CLOCK_50, CLOCK_27, KEY, FL_ADDR, FL_CE_N, FL_DQ, FL_OE_N, FL_RST_N, FL_WE_N,
+         AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK,AUD_ADCDAT, I2C_SDAT, I2C_SCLK, AUD_DACDAT, AUD_XCK);
+
+input CLOCK_50,CLOCK_27,AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK,AUD_ADCDAT;
 input [3:0] KEY;
+inout I2C_SDAT;
+output I2C_SCLK,AUD_DACDAT,AUD_XCK;
 
 // Flash memory I/O
 // low enable for all signals
@@ -19,11 +16,10 @@ output FL_RST_N;			// Flash hardware reset
 output FL_WE_N;			// Flash write enable
 
 // State machine states
-enum {FL_INIT, FL_READ, FL_WAIT, FL_LOAD, FL_LOOP_CONTROL, DONE} state;
+enum {FL_INIT, FL_READ, FL_WAIT, FL_LOAD, FL_LOOP_CONTROL, WAIT_READY, SEND_SAMPLE, WAIT_ACCEPTED, DONE} state;
 
 // clock & reset
-wire clk, resetb;
-assign clk = CLOCK_50;
+wire resetb;
 assign resetb = KEY[3];
 
 // Do not need to reset, nor write to flash
@@ -34,13 +30,25 @@ assign FL_RST_N = 1'b1; // flash reset is low enable
 parameter WAIT_CYCLES = 3'd6; // minimum 110ns read time, so wait 6 cycles in between read and load on 50MHz clock (120ns)
 reg [2:0] wait_reg;	// register for number of cycles in 
 
-// RAM & intermediary registers
-reg s_wren;
-reg [7:0] s_address;
-reg [15:0] s_data, s_q, f_data;
+reg [23:0] f_address;
+reg [15:0] f_data;
 reg addr_offset;
 
-s_memory s0(.address(s_address),
+// signals that are used to communicate with the audio core
+reg read_ready, write_ready, write_s;
+reg [15:0] writedata_left, writedata_right;
+reg [15:0] readdata_left, readdata_right;	
+wire audio_reset, read_s;
+
+assign audio_reset = ~(KEY[3]);
+assign read_s = 1'b0;
+
+// instantiate the parts of the audio core. 
+clock_generator my_clock_gen (CLOCK_27, audio_reset, AUD_XCK);
+audio_and_video_config cfg (CLOCK_50, audio_reset, I2C_SDAT, I2C_SCLK);
+audio_codec codec (CLOCK_50,audio_reset,read_s,write_s,writedata_left, writedata_right,AUD_ADCDAT,AUD_BCLK,AUD_ADCLRCK,AUD_DACLRCK,read_ready, write_ready,readdata_left, readdata_right,AUD_DACDAT);
+
+s_memory s0(.address(f_address),
 				.clock(CLOCK_50),
 				.data(s_data),
 				.wren(s_wren),
@@ -49,6 +57,7 @@ s_memory s0(.address(s_address),
 always_ff @(posedge CLOCK_50, negedge resetb) begin
 
 	if(resetb == 0) begin
+		write_s <= 1'b0;
 		state <= FL_INIT;
 	end else begin
 	
@@ -58,7 +67,7 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 		 * This state initializes registers needed for loading data from flash
 		 */
 		FL_INIT: begin
-			s_address <= 8'd0;	// initialize address to 0
+			f_address <= 8'd0;	// initialize address to 0
 			addr_offset <= 1'b0;	// initialize flash offset address to 0
 			wait_reg <= 3'd0;		// initialize wait counter to 0
 			
@@ -73,7 +82,7 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 			FL_CE_N <= 1'b0;
 			
 			// can only read 8 bits at a time, so read each byte separately
-			FL_ADDR <= (2'd2 * s_address) + addr_offset;
+			FL_ADDR <= (2'd2 * f_address) + addr_offset;
 			
 			state <= FL_WAIT;
 		end // case FL_READ
@@ -117,21 +126,42 @@ always_ff @(posedge CLOCK_50, negedge resetb) begin
 			end else begin
 				addr_offset <= 1'b0;
 				
-				s_wren <= 1'b1;
-				s_data <= f_data;
+				// start write handshake
 				
-				state <= FL_LOOP_CONTROL;
+				state <= WAIT_READY;
 			end // if
 		end // case FL_LOAD
+		
+		WAIT_READY: begin
+		
+			if (write_ready == 1'b1) begin
+				state <= SEND_SAMPLE;
+			end
+		end
+		
+		SEND_SAMPLE: begin
+		
+			writedata_left <= f_data;
+			writedata_right <= f_data;
+			write_s <= 1'b1;
+			state <= WAIT_ACCEPTED;
+		end
+		
+		WAIT_ACCEPTED: begin
+			if (write_ready == 1'b0) begin
+				state <= FL_LOOP_CONTROL;
+			end
+		end
 		
 		/* FL_LOOP_CONTROL
 		 * This state determines whether we need to continue reading information,
 		 * and updates the write address accordingly
 		 */
 		FL_LOOP_CONTROL: begin
+			write_s <= 1'b0;
 			
-			if (s_address < 8'd255) begin
-				s_address <= s_address + 1'b1;
+			if (f_address < 21'h1FFFFF) begin
+				f_address <= f_address + 1'b1;
 				state <= FL_READ;
 			end else begin
 				state <= DONE;
